057c3655e3f7d498a1f4caff2ebcba12728cd3fc
Karen Daniela López <Karen_lopez1936@hotmail.com>
2019-11-05 13:25:13 -0700

    llamado final del cuadrado

--- helloWorld.js
+++ helloWorld.js
@@ -87,9 +87,63 @@ const drawScene = (gl,programInfo, buffers)=>{
   const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
   const zNear = 0.1;
   const zFar = 100;
+
   const projectionMatrix = mat4.create();
 
-}
+  mat4.perspective(
+    projectionMatrix,
+   fieldOfView,
+    aspect,
+    zNear,
+    zFar
+  );
+
+  const modelViewMatrix = mat4.create();
+
+  mat4.translate(modelViewMatrix,   
+    modelViewMatrix,    
+    [-0, 0, -6]);
+
+    {
+      /*make quad*/
+
+      const numComponents = 2;
+      const type = gl.FLOAT;
+      const normalize = false;
+      const stride  = 0;
+      const offset = 0;     
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
+    gl.vertexAttribPointer(
+        programInfo.attribLocations.vertexPosition,
+        numComponents,
+        type,
+        normalize,
+        stride,
+        offset
+        );
+    gl.enableVertexAttribArray(
+        programInfo.attribLocations.vertexPosition);
+  }
+
+  gl.useProgram(programInfo.program);
+
+  gl.uniformMatrix4fv(
+      programInfo.uniformLocations.projectionMatrix,
+      false,
+      projectionMatrix);
+  gl.uniformMatrix4fv(
+      programInfo.uniformLocations.modelViewMatrix,
+      false,
+      modelViewMatrix);
+
+  {
+    const offset = 0;
+    const vertexCount = 4;
+    gl.drawArrays(gl.TRIANGLE_STRIP, offset, vertexCount);
+  }
+    }
+
 
 const main = ()=>{
     const gl = canvas.getContext('webgl2');
